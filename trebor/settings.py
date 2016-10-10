1a4b40cbb8011f8ffa4b38b605af7d6a371e6f59
Emmanuel Lodovice <name3anad@gmail.com>
2016-10-10 22:59:01 +0800

    added static root

--- trebor/settings.py
+++ trebor/settings.py
@@ -105,4 +105,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static2/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'storage/'
