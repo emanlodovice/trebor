28042527910083f138674581c2ac04ada53d3559
Emmanuel Lodovice <name3anad@gmail.com>
2016-10-11 00:14:54 +0800

    updated views

--- product/models.py
+++ product/models.py
@@ -19,5 +19,19 @@ class Product(models.Model):
     price = models.FloatField(null=True, blank=True)
     specs = models.TextField(blank=True)
 
+    def specs_list(self):
+        specs = self.specs.strip()
+        specs_list = []
+        if len(specs) > 0:
+            specs = specs.split(',')
+            for spec in specs:
+                s = spec.strip()
+                specs_list.append(s.split(':'))
+        return specs_list
+
+    @property
+    def cost(self):
+        return '%.2f' % self.price
+
     def __str__(self):
         return self.name
--- trebor/views.py
+++ trebor/views.py
@@ -1,14 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, ListView
+
+from product.models import Product
 
 
-class Home(TemplateView):
+class Home(ListView):
     template_name = 'trebor/home.html'
+    model = Product
 
 
-class Detail(TemplateView):
+class Detail(DetailView):
     template_name = 'trebor/detail.html'
+    model = Product
 
-    def get_context_data(self):
-        context = super(Detail, self).get_context_data()
+    def get_context_data(self, *args, **kwargs):
+        context = super(Detail, self).get_context_data(*args, **kwargs)
         context['url'] = self.request.build_absolute_uri()
         return context
